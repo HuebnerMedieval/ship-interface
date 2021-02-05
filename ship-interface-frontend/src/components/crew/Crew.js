7138998ed324f6da9ffe789d71841b54a417f270
elimhuebner@gmail.com <ehbner@DESKTOP-O6P2HM5.localdomain>
2021-02-05 15:00:09 -0500

    turns Crewmate component into functional component

--- ship-interface-frontend/src/components/crew/Crew.js
+++ ship-interface-frontend/src/components/crew/Crew.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Crewmate from './Crewmate'
 
 const Crew = props => {
-    // console.log(props.crew)
+
     return(
         <div id="crew-list">
             {props.crew.map(crewmate => <Crewmate key={crewmate.id} {...crewmate} removeCrew={props.removeCrew}/>)}
--- ship-interface-frontend/src/components/crew/Crewmate.js
+++ ship-interface-frontend/src/components/crew/Crewmate.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 import CrewButton from './CrewButton'
 
-class Crewmate extends Component {
-    
-    
-    render() {
-        return (
-            <div className="crewmate">
-                <li> Name: {this.props.name}
-                    <p>Role: {this.props.role}</p>
-                    <p>Status: {this.props.status}</p>
-                    < CrewButton removeCrew={this.props.removeCrew} id={this.props.id}/>
-                </li>
-            </div>
-        )
-    }
+const Crewmate = props => {
+    return (
+        <div className="crewmate">
+            <li> Name: {props.name}
+                <p>Role: {props.role}</p>
+                <p>Status: {props.status}</p>
+                < CrewButton removeCrew={props.removeCrew} id={props.id}/>
+            </li>
+        </div>
+    )
 }
 
 export default Crewmate
