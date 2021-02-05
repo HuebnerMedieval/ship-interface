820b1a9470f78a32aa7dc0d04dce1f5f5380ce72
elimhuebner@gmail.com <ehbner@DESKTOP-O6P2HM5.localdomain>
2021-02-05 17:34:08 -0500

    adds early code to set up routing

--- ship-interface-frontend/src/App.js
+++ ship-interface-frontend/src/App.js
@@ -4,6 +4,8 @@ import { fetchShip } from './actions/shipActions'
 import { fetchCrew } from './actions/crewActions'
 import ShipParts from './containers/ShipParts'
 import CrewContainer from './containers/CrewContainer'
+import CrewForm from './components/crew/CrewForm'
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 
 class App extends Component {
   componentDidMount() {
@@ -13,13 +15,26 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <h1 id="heading">Roci-NOT-eh Ship's Interface</h1>
-        <hr/>
-        < ShipParts />
-        <hr/>
-        < CrewContainer />
-      </div>
+      <Router>
+        <div className="App">
+          {/* need to make Navigation with links to ShipParts and Crew */}
+          <Navigation/>
+          <h1 id="heading">Roci-NOT-eh Ship's Interface</h1>
+          <hr/>
+          < ShipParts />
+          <hr/>
+          < CrewContainer />
+          <Switch>
+            {/* Move heading into Home */}
+            <Route exact path="/" component={Home}/>
+            {/* remove ShipParts */}
+            <Route exact path="/ship" component={ShipParts}/>
+            {/* remove CrewContainer and move CrewForm out*/}
+            <Route exact path="/crew" component={CrewContainer}/>
+            <Route exact path="/crew/new" component={CrewForm}/>
+          </Switch>
+        </div>
+      </Router>
     )
   }
 }
--- ship-interface-frontend/src/index.js
+++ ship-interface-frontend/src/index.js
@@ -6,7 +6,6 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-// this will be rootReducer
 import rootReducer from './reducers/rootReducer'
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
