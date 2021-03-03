4431ed9a5a373103509ce44b11828f4503078b3b
elimhuebner@gmail.com <ehbner@DESKTOP-O6P2HM5.localdomain>
2021-03-03 09:33:30 -0500

    removed some used code

--- ship-interface-frontend/src/components/crew/Crew.js
+++ ship-interface-frontend/src/components/crew/Crew.js
@@ -3,11 +3,14 @@ import Crewmate from './Crewmate'
 
 const Crew = props => {
 
+
+
     return(
         <div id="crew-list">
             {props.crew.map(crewmate => <Crewmate key={crewmate.id} {...crewmate} removeCrew={props.removeCrew}/>)}
         </div>
     )
 }
+
 
 export default Crew
