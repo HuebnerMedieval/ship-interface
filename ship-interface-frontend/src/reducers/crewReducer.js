a77a0573ebb88ecc42dcfaee1c6ddc0f33fd9677
Eli Huebner <elimhuebner@gmail.com>
2021-04-09 10:34:58 -0400

    adds comments to crewReducer.js

--- ship-interface-frontend/src/reducers/crewReducer.js
+++ ship-interface-frontend/src/reducers/crewReducer.js
@@ -1,11 +1,14 @@
+// reducer for all crew related actions
 const crewReducer = (state={crew: [], loading: false }, action) => {
     switch(action.type) {
+        // sets status to loading
         case 'LOADING_CREW':
             return {
                 ...state,
                 loading: true
             }
 
+        // recieves data on all crewmembers and adds them to the store
         case 'UPDATE_CREW':
             return {
                 ...state,
@@ -13,12 +16,14 @@ const crewReducer = (state={crew: [], loading: false }, action) => {
                 loading: false
             }
 
+        // sets loading status to true for adding crew members
         case 'ADD_CREW':
             return {
                 ...state,
                 loading: true
             }
 
+        // adds new crewmember to the store and resets loading status
         case 'CREW_ADDED':
             return {
                 ...state,
@@ -26,12 +31,14 @@ const crewReducer = (state={crew: [], loading: false }, action) => {
                 loading: false
             }
         
+        // sets loading status to true for removing crew members
         case 'REMOVE_CREW':
             return {
                 ...state,
                 loading: true
             }
 
+        // removes crew member from store and resets loading status
         case 'CREW_REMOVED':
             const crew = state.crew.filter(crew => crew.id !== action.payload)
             return{
