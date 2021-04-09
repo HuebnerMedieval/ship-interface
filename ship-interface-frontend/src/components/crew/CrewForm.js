1ba5f70dffe0b12746d11c18b53b9aa5bd913e16
Eli Huebner <elimhuebner@gmail.com>
2021-04-09 10:19:15 -0400

    adds comments to CrewForm.js

--- ship-interface-frontend/src/components/crew/CrewForm.js
+++ ship-interface-frontend/src/components/crew/CrewForm.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
 
+// form for adding new crew member
 class CrewForm extends Component {
+    // empty state for a controlled form
     state = {
         crew: {
             name: "",
             role: "",
             status: "green"
         },
+        // loading status, in cases needed later
         loading: false
     }
 
+    // handles the controlled form, updating state on button press
     handleChange = event => {
         this.setState({...this.state,
             crew: {...this.state.crew,
@@ -18,10 +22,13 @@ class CrewForm extends Component {
         })
     }
 
+    // handles behavior on form submission
     handleSubmit = event => {
         event.preventDefault()
+        // saves form data/state as variable to send with the addCrew dispatch
         const crewmate = {...this.state.crew}
         this.props.addCrew(crewmate)
+        // resets state to clear the form
         this.setState({
             crew: {
                 name: "",
