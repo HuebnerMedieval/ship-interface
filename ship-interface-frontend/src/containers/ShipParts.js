9104e0415e9c697363ebe2f2d46869056f08970c
Eli Huebner <elimhuebner@gmail.com>
2021-04-09 10:31:44 -0400

    adds comments to ShipParts.js

--- ship-interface-frontend/src/containers/ShipParts.js
+++ ship-interface-frontend/src/containers/ShipParts.js
@@ -6,13 +6,16 @@ import Weapons from '../components/ship/Weapons'
 import { updateShip } from '../actions/shipActions'
 import Buttons from '../components/ship/Buttons'
 
+// holds all components relating to the ship
 class ShipParts extends Component {
 
-
+    // bandaid solution to ship updating
+    // after the store updates the ship status, tells the backend to update as well
     componentDidUpdate(){
         this.props.updateShip(this.props.ship)
     }
 
+    // renders components for ship systems
     render(){
         return (
             <div className="container">
@@ -31,13 +34,15 @@ class ShipParts extends Component {
 
 }
 
+// adds global state to the component's props
 const mapStateToProps = state => {
     return {
       ship: state.ship.ship
     }
 }
 
-  const mapDispatchToProps = dispatch => {
+// adds dispatch functions to props
+const mapDispatchToProps = dispatch => {
     return {
         applyDamage: (system) => dispatch({type: 'APPLY_DAMAGE', payload: system}),
         applyRepair: (system) => dispatch({type: 'APPLY_REPAIR', payload: system}),
