5d4c26c4055e9dcbd26d0b16be799850771287ab
Eli Huebner <elimhuebner@gmail.com>
2021-04-09 10:14:43 -0400

    adds comments to Crew.js

--- ship-interface-frontend/src/components/crew/Crew.js
+++ ship-interface-frontend/src/components/crew/Crew.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import Crewmate from './Crewmate'
 
+// renders the list of crew
 const Crew = props => {
 
-
-
+    // iterates through the crew passed down as props, rendering a <Crewmate> for each
     return(
         <div id="crew-list">
             {props.crew.map(crewmate => <Crewmate key={crewmate.id} {...crewmate} removeCrew={props.removeCrew}/>)}
