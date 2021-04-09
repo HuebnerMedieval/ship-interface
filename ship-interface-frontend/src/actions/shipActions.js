0ed5c9b18a93f48ad27101ca6deea819906404a8
Eli Huebner <elimhuebner@gmail.com>
2021-04-09 10:13:08 -0400

    adds comments to shipActions.js

--- ship-interface-frontend/src/actions/shipActions.js
+++ ship-interface-frontend/src/actions/shipActions.js
@@ -1,5 +1,7 @@
+// saves ship show route as a constant
 const shipURL = "http://localhost:3001/ships/1"
 
+// sents a get request to the backend, then dispatches an 'UPDATE_SHIP' action to the store with the current ship data
 export const fetchShip = () => {
     
     return (dispatch) => {
@@ -10,6 +12,10 @@ export const fetchShip = () => {
     }
 }
 
+// takes ship data from the store, and sends it to the backend to update the database
+/*ideal solution would be to send data directly to the backend, then take that data to update store
+opening dispatch action breaks the rest of the code, and the easiest solution for form management was
+to update the store first. May refactor later to fit convention*/
 export const updateShip = (ship) => {
     return () => {
         // dispatch({type: "LOADING_SHIP"})
